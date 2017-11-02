5f3e39cc3938985c258ff59402dd0dcb5e92dce1
Niklas Dreymann <ge56pos@mytum.de>
2017-11-02 19:31:47 +0100

    HelloUser is added

---
+++ src/HelloUser.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class HelloUser {
+	public static void greetUser (String username) {
+		
+		System.out.println("Hello " + username + "!");
+		
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello ASE2017 - How are you? :)");
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Please enter your name:");
+		String username = sc.next();
+		HelloUser hu = new HelloUser ();
+		hu.greetUser(username);
 	}
 
 }
